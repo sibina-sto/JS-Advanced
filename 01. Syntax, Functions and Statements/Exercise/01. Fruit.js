2b651514fbf2742cd61d67003bb395ff8dc54d61
piziamko <stoyanovasibina@gmail.com>
2021-08-15 12:39:40 +0300

    Create 01. Fruit.js

---
+++ 01. Syntax, Functions and Statements/Exercise/01. Fruit.js
@@ -0,0 +1,6 @@
+function solve(type, grams, price){
+    const kg = grams / 1000;
+    console.log(`I need $${(price * kg).toFixed(2)} to buy ${kg.toFixed(2)} kilograms ${type}.`)
+}
+
+solve('orange', 2500, 1.80);
