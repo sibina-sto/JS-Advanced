6176f725c627c2c8dde894622f918d2bdefcd39d
piziamko <stoyanovasibina@gmail.com>
2021-08-15 12:51:27 +0300

    Create 06. Cooking by Numbers.js

---
+++ 01. Syntax, Functions and Statements/Exercise/06. Cooking by Numbers.js
@@ -0,0 +1,30 @@
+function solve(...input) {
+    let num = Number(input.shift());
+
+    const cooking = {
+        chop() {
+            num /= 2;
+        },
+        dice() {
+            num = Math.sqrt(num);
+        },
+        spice() {
+            num++;
+        },
+        bake() {
+            num *= 3;
+        },
+        fillet() {
+            num = (num * 0.8).toFixed(1);
+        },
+    };
+
+    for (let el of input) {
+        cooking[el]();
+        console.log(num);
+    }
+}
+
+solve('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+
+solve('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
