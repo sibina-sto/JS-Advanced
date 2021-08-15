8c8523d04e9d017badcba04a10c9afb856b9e82c
piziamko <stoyanovasibina@gmail.com>
2021-08-15 12:32:38 +0300

    Create 07. Day of week.js

---
+++ 01. Syntax, Functions and Statements/Lab/07. Day of week.js
@@ -0,0 +1,16 @@
+function solve(day){
+    let result = {
+        'Monday': 1,
+        'Tuesday': 2,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5,
+        'Saturday': 6,
+        'Sunday': 7
+    }
+
+    return (result[day] ? result[day] : 'error')
+}
+
+console.log(solve('Monday'));
+console.log(solve('asd'));
