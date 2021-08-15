3e7b74a43c99943a7a8c08ce41e6e070035d40ad
piziamko <stoyanovasibina@gmail.com>
2021-08-15 12:27:20 +0300

    Create 02. String length.js

---
+++ 01. Syntax, Functions and Statements/Lab/02. String length.js
@@ -0,0 +1,7 @@
+function solve(first, second, third){
+    const result = first.length + second.length + third.length;
+    console.log(result);
+    console.log(Math.floor( result / 3 ));
+}
+
+solve('chocolate', 'ice cream', 'cake');
