4c3d1d1cb1a3422e7c981706bfd559546210ef57
piziamko <stoyanovasibina@gmail.com>
2021-08-15 12:30:07 +0300

    Create 06.Sum of numbers from N to M.js

---
+++ 01. Syntax, Functions and Statements/Lab/06.Sum of numbers from N to M.js
@@ -0,0 +1,13 @@
+function solve(first, second){
+    first = Number(first);
+    second = Number(second);
+    let result = 0;
+
+    for (let i = first; i <= second; i++){
+        result += i;
+    }
+
+    return result;
+}
+
+console.log(solve('-8','20'));
