4baaba6d0788cfed765e0382549d6ca6e8c22089
piziamko <stoyanovasibina@gmail.com>
2021-09-15 22:18:46 +0300

    Create 02 .Last K Numbers Sequence.js

---
+++ 02. Arrays and Nested Arrays/Lab/02 .Last K Numbers Sequence.js
@@ -0,0 +1,13 @@
+//2.	Last K Numbers Sequence
+function lastK(n , k) {
+    let result = [1];
+    for (let i = 1; i < n; i++) {
+          let startIndex = Math.max(0,i-k);
+          let currentElement = result.slice(startIndex,startIndex + k).reduce((a,b)=> a+b ,0);
+          result.push(currentElement);
+    }
+
+    return result;
+}
+
+// console.log(lastK(6, 3));
