c0ee8e6c073e4f242659a07d8058d5f790b3c0a9
piziamko <stoyanovasibina@gmail.com>
2021-09-15 22:16:35 +0300

    Update 01. Even Position Element.js

--- 02. Arrays and Nested Arrays/Lab/01. Even Position Element.js
+++ 02. Arrays and Nested Arrays/Lab/01. Even Position Element.js
@@ -1,10 +1,12 @@
-function solve(input) {
-    let arr = [];
-    for (let i = 0; i <= input.length; i += 2) {
-        arr.push(input[i]);
+//1.	Even Position Element
+function evenPosition(arr) {
+    let sum = '';
+    for (let i = 0; i < arr.length; i+= 2) {
+     sum += arr[i] + " ";
+
     }
+   return sum;
+} 
 
-    console.log(arr.join(' '));
-}
-
-solve(['20', '30', '40', '50', '60']);
+// console.log(evenPosition(['20', '30', '40', '50', '60']));
+// evenPosition(['5', '10']);
