06a2b3c38dd8dba9f6702224133833b9b7f9295b
piziamko <stoyanovasibina@gmail.com>
2021-09-15 21:35:34 +0300

    Create 04. Negative Positive Numbers.js

---
+++ 02. Arrays and Nested Arrays/Lab/04. Negative Positive Numbers.js
@@ -0,0 +1,15 @@
+function solve(arr) {
+    const result = [];
+
+    for (let el of arr) {
+        if (el < 0) {
+            result.unshift(el);
+        } else {
+            result.push(el);
+        }
+    }
+
+    console.log(result.join('\n'));
+}
+
+solve([7, -2, 8, 9]);
