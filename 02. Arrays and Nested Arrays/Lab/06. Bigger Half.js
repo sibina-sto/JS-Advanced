05a675ec39767f603947c88b9d8f62043d2b7d98
piziamko <stoyanovasibina@gmail.com>
2021-09-15 21:37:38 +0300

    Create 06. Bigger Half.js

---
+++ 02. Arrays and Nested Arrays/Lab/06. Bigger Half.js
@@ -0,0 +1,9 @@
+function solve(arr) {
+    const result = arr.sort((a, b) => a - b);
+
+    console.log(`[${result.splice(Math.ceil(result.length / 2)).join(', ')}]`);
+}
+
+//solve([3, 19, 14, 7, 2, 19, 6]);
+
+solve([3, 19, 14, 7, 2, 19, 6]);
