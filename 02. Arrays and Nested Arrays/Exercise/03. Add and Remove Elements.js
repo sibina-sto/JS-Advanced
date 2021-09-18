a568b88042f0af9bf810b44c431a62119a9d456c
piziamko <stoyanovasibina@gmail.com>
2021-09-18 15:11:24 +0300

    Update 03. Add and Remove Elements.js

--- 02. Arrays and Nested Arrays/Exercise/03. Add and Remove Elements.js
+++ 02. Arrays and Nested Arrays/Exercise/03. Add and Remove Elements.js
@@ -1,5 +1,19 @@
-//3.	Add and Remove Elements  
+//3.	Add and Remove Elements
 
+function foo(arr) {
+    let count = 1
+    const temp = []
+    const actions = {
+        add: x => temp.push(x),
+        remove: () => temp.pop(),
+    }
+
+    arr.forEach(x => actions[x](count++))
+    return temp.length !== 0 ? temp.join("\n") : "Empty"
+}
+
+
+//////
 function addAndRemove(commands) {
     let result = [];
     let number = 1;
