1aa9618ddd85f59a5f0787ee69e4ea675e8d457f
piziamko <stoyanovasibina@gmail.com>
2021-10-10 13:42:45 +0300

    Create 01. Sort-array.js

---
+++ 06. Advanced Functions/Exercise/01. Sort-array.js
@@ -0,0 +1,8 @@
+function sort(array, argument) {
+    const sort = {
+        asc: (array) => array.sort((a, b) => a - b),
+        desc: (array) => array.sort((a, b) => b - a)
+    };
+
+    return sort[argument](array);
+}
