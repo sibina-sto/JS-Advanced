8a973550415872a178d77478ab467e8181b1960c
piziamko <stoyanovasibina@gmail.com>
2021-10-10 22:38:18 +0300

    Create 05. Command Processor.js

---
+++ 06. Advanced Functions/Lab/05. Command Processor.js
@@ -0,0 +1,10 @@
+function solve() {
+    let str = ""
+
+    return {
+        append: v => (str += v),
+        removeStart: x => (str = str.slice(x)),
+        removeEnd: x => (str = str.slice(0, -x)),
+        print: () => console.log(str),
+    }
+}
