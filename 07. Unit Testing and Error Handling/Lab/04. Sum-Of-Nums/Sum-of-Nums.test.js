db2beea4f621275ca763b8672edcf5cc1339960f
piziamko <stoyanovasibina@gmail.com>
2021-10-16 10:03:16 +0300

    Create Sum-of-Nums.test.js

---
+++ 07. Unit Testing and Error Handling/Lab/04. Sum-Of-Nums/Sum-of-Nums.test.js
@@ -0,0 +1,9 @@
+// const { expect } = require('chai')
+// const sum = require('./SumOfNums')
+
+describe('sum numbers', () => {
+    it('sums single number', () => {
+        expect(sum([1])).to.equal(1);
+        
+    });
+});
